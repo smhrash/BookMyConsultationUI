66578336f2caa6aa9d2334f080f186af9ab58db3
Vidyasagar Kumar <vksagar82@gmail.com>
2024-01-23 00:25:09 +0530

    Home js update

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,34 @@
 import Header from "../../common/header/Header";
-import React from "react";
+import React, { useState } from "react";
+import { Tab, Tabs } from "@material-ui/core";
+import TabContainer from "../../common/tabContainer/TabContainer";
 
 const Home = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
   return (
     <div>
       <Header></Header>
+      <Tabs
+        variant="fullWidth"
+        value={activeTab}
+        indicatorColor="primary"
+        onChange={handleTabChange}
+        centered
+      >
+        <Tab label="Doctors"></Tab>
+        <Tab label="Appointment"></Tab>
+      </Tabs>
+      <TabContainer value={activeTab} index={0}>
+        Doctors
+      </TabContainer>
+      <TabContainer value={activeTab} index={1}>
+        Login to see Appointments
+      </TabContainer>
     </div>
   );
 };
